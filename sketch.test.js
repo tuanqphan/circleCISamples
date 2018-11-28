7cf712d241e274298fecfb4da2ead4617aae7bad
tuanqphan <tuanqphan@yahoo.com>
2018-11-27 18:07:06 -0800

    adding more tests

--- sketch.test.js
+++ sketch.test.js
@@ -1,5 +1,5 @@
 
-const sum = require('./sketch');
+const {sum, subtraction }= require('./sketch');
 
 // test('adds 1 + 2 to equal 3', () => {
 //   expect(sum(1, 2)).toBe(3);
@@ -7,7 +7,13 @@ const sum = require('./sketch');
 
 test('adds 1 + 2 to equal 3', sumTest);
 
+test('subtract 10 from 100 equal 90', subtractionTest);
+
+
 function sumTest() {
     expect(sum(12, 2)).toBe(14);
 }
 
+function subtractionTest() {
+    expect(subtractionTest(100, 10)).toBe(90);
+}
