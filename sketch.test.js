5410e4a55b15df4b6a5e5f116669fec4e2e29fdb
tuanqphan <tuanqphan@yahoo.com>
2018-11-29 15:07:58 -0800

    adding more tests

--- sketch.test.js
+++ sketch.test.js
@@ -1,5 +1,5 @@
 
-const {sum, subtraction }= require('./sketch');
+const {sum, subtraction, getName }= require('./sketch');
 
 // test('adds 1 + 2 to equal 3', () => {
 //   expect(sum(1, 2)).toBe(3);
@@ -9,6 +9,8 @@ test('adds 1 + 2 to equal 3', sumTest);
 
 test('subtract 10 from 100 equal 90', subtractionTest);
 
+test('input name should be the same as output', getNameTest)
+
 
 function sumTest() {
     expect(sum(12, 2)).toBe(14);
@@ -16,4 +18,8 @@ function sumTest() {
 
 function subtractionTest() {
     expect(subtraction(100, 10)).toBe(90);
+}
+
+function getNameTest(){
+    expect(getName("Tuan")).toBe("Tuan");
 }
