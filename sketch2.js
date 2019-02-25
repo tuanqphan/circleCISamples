60c18919e6a3a74a3710bfa6286e6f7584981cea
tuanqphan <tuanqphan@yahoo.com>
2019-02-25 14:49:10 -0800

    Added second file and test

---
+++ sketch2.js
@@ -0,0 +1,17 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function subtraction(a, b) {
+    return a - b;
+}
+
+function getName(name) {
+    return name;
+}
+
+module.exports = {
+   sum: sum,
+   subtraction: subtraction,
+   getName: getName,
+}
---
+++ sketch2.test.js
@@ -0,0 +1,25 @@
+
+const {sum, subtraction, getName }= require('./sketch2');
+
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+
+test('Adds 1 + 2 to equal 3', sumTest);
+
+test('Subtract 10 from 100 equal 90', subtractionTest);
+
+test('Input name should be the same as output', getNameTest)
+
+
+function sumTest() {
+    expect(sum(12, 2)).toBe(14);
+}
+
+function subtractionTest() {
+    expect(subtraction(100, 10)).toBe(90);
+}
+
+function getNameTest(){
+    expect(getName("Tuan")).toBe("Tuan");
+}
